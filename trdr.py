95b08fc8a9604368f8856b4498e6f2eb202d2917
Enan456 <enan332@gmail.com>
2017-11-01 07:52:00 -0500

    Create trdr.py

---
+++ trdr.py
@@ -0,0 +1,10 @@
+# http://docs.python-requests.org/en/master/
+# https://github.com/ericsomdahl/python-bittrex/blob/master/bittrex/bittrex.py
+from bittrex import Bittrex
+import time
+TIME = 20
+MARGIN_UP = 55
+MARGIN_DOWN = 25
+API_KEY =  0
+def defTrdr():
+  
