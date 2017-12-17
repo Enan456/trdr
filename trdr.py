236b31d0e18201a6e4ed0c2eb72a834921d628c1
Enan Srivastava <enan332@gmail.com>
2017-12-16 22:01:22 -0600

    Update trdr.py

--- trdr.py
+++ trdr.py
@@ -6,5 +6,57 @@ TIME = 20
 MARGIN_UP = 55
 MARGIN_DOWN = 25
 API_KEY =  0
-def baseTrdr():
+# Step 1: For each node prepare the destination and predecessor
+def initialize(graph, source):
+    d = {} # Stands for destination
+    p = {} # Stands for predecessor
+    for node in graph:
+        d[node] = float('Inf') # We start admiting that the rest of nodes are very very far
+        p[node] = None
+    d[source] = 0 # For the source we know how to reach
+    return d, p
+ 
+def relax(node, neighbour, graph, d, p):
+    # If the distance between the node and the neighbour is lower than the one I have now
+    if d[neighbour] > d[node] + graph[node][neighbour]:
+        # Record this lower distance
+        d[neighbour]  = d[node] + graph[node][neighbour]
+        p[neighbour] = node
+ 
+def retrace_negative_loop(p, start):
+	arbitrageLoop = [start]
+	next_node = start
+	while True:
+		next_node = p[next_node]
+		if next_node not in arbitrageLoop:
+			arbitrageLoop.append(next_node)
+		else:
+			arbitrageLoop.append(next_node)
+			arbitrageLoop = arbitrageLoop[arbitrageLoop.index(next_node):]
+			return arbitrageLoop
+
+
+def bellman_ford(graph, source):
+    d, p = initialize(graph, source)
+    for i in range(len(graph)-1): #Run this until is converges
+        for u in graph:
+            for v in graph[u]: #For each neighbour of u
+                relax(u, v, graph, d, p) #Lets relax it
+
+
+    # Step 3: check for negative-weight cycles
+    for u in graph:
+        for v in graph[u]:
+        	if d[v] < d[u] + graph[u][v]:
+        		return(retrace_negative_loop(p, source))
+    return None
+
+paths = []
+
+graph = download()
+
+for key in graph:
+	path = bellman_ford(graph, key)
+	if path not in paths and not None:
+		paths.append(path)
   
